22a2922b08412fa2eef6484203d72c50e4cf2165
Peshk0 <petar.tonev.pp25@pgmtplovdiv.info>
2025-05-20 09:36:35 +0300

    Pong!

--- Pong/Game.cs
+++ Pong/Game.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Vector2 = System.Numerics.Vector2;
 
 namespace Pong
 {
+    public class Paddel
+    {
+        public Vector2 Scale = new Vector2(20, 70);
+        public Vector2 position = new Vector2(0,0);
+
+    }
     public class Game : Microsoft.Xna.Framework.Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Paddel p1;
+        Paddel p2;
         Texture2D ballTexture;
         Vector2 ballPosition;
+        Vector2 ballVelocity;
+        Vector2 mousePos;
         float ballSpeed;
+        bool down;
         //TODO: Добавете нова булева променлива down, която показва дали топката се движи нагоре/надолу
 
         public Game()
@@ -25,10 +36,13 @@ namespace Pong
         {
             ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
                                        _graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 100f;
+            ballSpeed = 300f;
 
+            ballVelocity = new Vector2(0, 0);
             //TODO: инициализирайте булевата променлива
-
+            p1 = new Paddel();
+            p2 = new Paddel();
+            p2.position = new Vector2(790, 0);
             base.Initialize();
         }
 
@@ -36,6 +50,7 @@ namespace Pong
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             ballTexture = Content.Load<Texture2D>("ball");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,12 +58,54 @@ namespace Pong
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
+            if(Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                p1.position.Y += 4f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                p1.position.Y -= 4f;
+            }
+
+            p2.position.Y = ballPosition.Y-p2.Scale.Y/3;
+
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                mousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+                ballVelocity = mousePos - ballPosition;
+            }
             float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             //TODO: Изменете ballPosition.Y координатата в зависимост от стойността на булевата променлива down
+            ballPosition += ballVelocity;
+            if (ballPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 /*|| ballPosition.X < 0*/)
+            {
+                ballVelocity.X = -ballVelocity.X;
+            }
+            else
+            if (p1.position.X+p1.Scale.X > ballPosition.X && p1.position.X < ballPosition.X && p1.position.Y < ballPosition.Y && p1.position.Y + p1.Scale.Y > ballPosition.Y)
+            {
+                ballVelocity.X = -ballVelocity.X;
 
-            //TODO: Ако картинката е напуска границите на екрана, това означава, че топката се е "ударила" в края на екрана и трябва да
-            //промени посоката си на движение. За целта трябва да промените променливата down.
+            }
+            if (ballPosition.Y > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2 || ballPosition.Y < 0)
+            {
+                ballVelocity.Y = -ballVelocity.Y;
+            }
+            if (ballPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 /*|| ballPosition.X < 0*/)
+            {
+                ballVelocity.X = -ballVelocity.X;
+            }
+            else
+            if (p2.position.X + p2.Scale.X > ballPosition.X && p2.position.X < ballPosition.X && p2.position.Y < ballPosition.Y && p2.position.Y + p2.Scale.Y > ballPosition.Y)
+            {
+                ballVelocity.X = -ballVelocity.X;
+
+            }
+            
+
 
 
             base.Update(gameTime);
@@ -66,12 +123,20 @@ namespace Pong
                 Color.White,
                 0f,
                 new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
-                Vector2.One,
+                0.3f,
                 SpriteEffects.None,
                 0f
             );
             _spriteBatch.End();
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), p2.position, null, Color.White, 0f, Vector2.One, p2.Scale / 20, SpriteEffects.None, 0f);
 
+            _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), p1.position, null, Color.White, 0f, Vector2.One, p1.Scale/20, SpriteEffects.None, 0f);
+            _spriteBatch.End();
+
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(Content.Load<SpriteFont>("file"), ballPosition.ToString(), Vector2.Zero, Color.White);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
