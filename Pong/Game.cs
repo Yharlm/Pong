8afc6bab63b219b1f33d34e28b0acdaa0098a41b
Peshk0 <petar.tonev.pp25@pgmtplovdiv.info>
2025-05-29 09:40:14 +0300

    E

--- Pong/Game.cs
+++ Pong/Game.cs
@@ -8,8 +8,9 @@ namespace Pong
     public class Paddel
     {
         public Vector2 Scale = new Vector2(20, 70);
-        public Vector2 position = new Vector2(0,0);
+        public Vector2 position = new Vector2(10, 0);
 
+        
     }
     public class Game : Microsoft.Xna.Framework.Game
     {
@@ -17,12 +18,13 @@ namespace Pong
         private SpriteBatch _spriteBatch;
         Paddel p1;
         Paddel p2;
+
         Texture2D ballTexture;
         Vector2 ballPosition;
         Vector2 ballVelocity;
-        
+        Vector2 ball_hit;
         Vector2 mousePos;
-        float ballSpeed;
+
         bool down;
         //TODO: Добавете нова булева променлива down, която показва дали топката се движи нагоре/надолу
 
@@ -37,7 +39,7 @@ namespace Pong
         {
             ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
                                        _graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 300f;
+
 
             ballVelocity = new Vector2(0, 0);
             //TODO: инициализирайте булевата променлива
@@ -60,7 +62,7 @@ namespace Pong
                 Exit();
 
 
-            if(Keyboard.GetState().IsKeyDown(Keys.S))
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
             {
                 p1.position.Y += 14f;
             }
@@ -69,13 +71,13 @@ namespace Pong
                 p1.position.Y -= 14f;
             }
 
-            
+            //Raycast
             for (int i = 0; i < 60; i++)
             {
                 if ((ballPosition + (ballVelocity * i)).X >= GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 - 10)
                 {
-                    p2.position.Y = ((ballPosition + (ballVelocity * i)).Y) - p2.Scale.Y / 2;
 
+                    ball_hit.Y = ((ballPosition + (ballVelocity * i)).Y) - p2.Scale.Y / 2;
                     break;
 
                 }
@@ -87,47 +89,54 @@ namespace Pong
                 mousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
                 ballVelocity = mousePos - ballPosition;
             }
-            float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             //TODO: Изменете ballPosition.Y координатата в зависимост от стойността на булевата променлива down
             ballPosition += ballVelocity;
 
-            
-            if (ballPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 || ballPosition.X < 0)
-            {
-                ballVelocity.X = -ballVelocity.X;
-            }
-            else
-            if (p1.position.X + p1.Scale.X > ballPosition.X && p1.position.X < ballPosition.X && p1.position.Y < ballPosition.Y && p1.position.Y + p1.Scale.Y > ballPosition.Y)
-            {
-                ballVelocity.X = -ballVelocity.X;
-
-                ballVelocity.Y = p1.position.Y+p1.Scale.Y/2 - ballPosition.Y;
 
 
 
-            }
-            if (ballPosition.Y > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2 || ballPosition.Y < 0)
-            {
-                ballVelocity.Y = -ballVelocity.Y;
-            }
-            if (ballPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 || ballPosition.X < 0)
-            {
-                ballVelocity.X = -ballVelocity.X;
-            }
-            else
-            if (p2.position.X + p2.Scale.X > ballPosition.X && p2.position.X < ballPosition.X && p2.position.Y < ballPosition.Y && p2.position.Y + p2.Scale.Y > ballPosition.Y)
-            {
-                ballVelocity.X = -ballVelocity.X;
 
-            }
-            
+            //Window it borders
+
+            Collisions();
+
+
 
 
 
             base.Update(gameTime);
         }
 
+        private void Collisions()
+        {
+            if (ballPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 || ballPosition.X < 0)
+            {
+                //ballVelocity.X = -ballVelocity.X;
+            }
+            else if (ballPosition.Y > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2 || ballPosition.Y < 0)
+            {
+                ballVelocity.Y = -ballVelocity.Y;
+            }
+            if (ballPosition.X < p1.position.X + p1.Scale.X || ballPosition.X > p2.position.X)
+            {
+                ballVelocity.X = p1.HitBall(ballVelocity, ballPosition);
+                ballVelocity.X = p2.HitBall(ballVelocity, ballPosition);
+            }
+
+
+        }
+        private void Movebot()
+        {
+            if (p2.position.Y < ball_hit.Y)
+            {
+                p2.position.Y += 0.1f;
+            }
+            else
+            {
+                p2.position.Y -= 0.1f;
+            }
+        }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
@@ -135,7 +144,7 @@ namespace Pong
             _spriteBatch.Begin();
             _spriteBatch.Draw(
                 ballTexture,
-                ballPosition+Vector2.One* 0.3f,
+                ballPosition + Vector2.One * 0.3f,
                 null,
                 Color.White,
                 0f,
@@ -149,19 +158,21 @@ namespace Pong
             _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), p2.position, null, Color.White, 0f, Vector2.One, 1, SpriteEffects.None, 0f);
             _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), p2.position, null, Color.White, 0f, Vector2.One, 1, SpriteEffects.None, 0f);
 
+            _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), new Vector2(p1.position.X, p1.position.Y), null, Color.Wheat, 0f, Vector2.One, p1.Scale / 14, SpriteEffects.None, 0f);
+
             _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), new Vector2(p1.position.X + p1.Scale.X, p1.position.Y), null, Color.White, 0f, Vector2.One, 0.3f, SpriteEffects.None, 0f);
-            _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), new Vector2(p1.position.X+p1.Scale.X,p1.position.Y+p1.Scale.Y), null, Color.Red, 0f, Vector2.One, 0.3f, SpriteEffects.None, 0f);
-            for (int i = 0;i < 60;i++)
+            _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), new Vector2(p1.position.X + p1.Scale.X, p1.position.Y + p1.Scale.Y), null, Color.Red, 0f, Vector2.One, 0.3f, SpriteEffects.None, 0f);
+            for (int i = 0; i < 60; i++)
             {
-                if((ballPosition + (ballVelocity * i)).X >= GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width/2-10)
+                if ((ballPosition + (ballVelocity * i)).X >= GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 - 10)
                 {
-                    
+
 
                     break;
 
                 }
                 _spriteBatch.Draw(Content.Load<Texture2D>("dirt"), ballPosition + (ballVelocity * i), null, Color.Red, 0f, Vector2.One, 0.5f, SpriteEffects.None, 0f);
-            }    
+            }
 
             _spriteBatch.End();
 
